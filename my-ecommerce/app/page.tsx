21ce84af885b4fd6876c2f0d295588ef7db98b24
mesh-magara <meshackmagara75@gmail.com>
2026-06-22 16:34:48 +0300

    file creation for the the projects

---
+++ my-ecommerce/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-amber-300 text-4xl">
+      <main>
+        <h1>Welcome to the ecommerce website</h1>
+      </main>
+    </div>
+  );
+}
